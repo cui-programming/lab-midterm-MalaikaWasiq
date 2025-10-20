4094c899f92a25ec30e9a61912632f80ffd3b6ba
MalaikaWasiq <malaikaqazi24@gmail.com>
2025-10-20 09:11:40 +0500

    Update student.js

--- src/config/student.js
+++ src/config/student.js
@@ -1,4 +1,10 @@
-// Edit these two fields with your details before you start.
-// They will be shown at the very top of the screen.
-export const STUDENT_NAME = "Your Name";
-export const REG_NO = "REG-000000";
+export const STUDENT_NAME = "Malaika Wasiq";
+export const REG_NO = "SP23-BSE-041";
+
+export const initialAzkaar = [
+  { id: '1', phrase: 'SubḥānAllāh', count: 0 },
+  { id: '2', phrase: 'Alḥamdulillāh', count: 0 },
+  { id: '3', phrase: 'Allāhu Akbar', count: 0 },
+  { id: '4', phrase: 'Lā ilāha illā Allāh', count: 0 },
+  { id: '5', phrase: 'Astaghfirullāh', count: 0 },
+];
