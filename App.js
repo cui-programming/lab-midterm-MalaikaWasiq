0cc70f09842a6d86b3a1004b4d4a78c6c0d22ad3
MalaikaWasiq <malaikaqazi24@gmail.com>
2025-10-20 09:10:15 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { View, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView } from 'react-native';
 import { styles } from './src/styles/styles';
 import AboutMe from './src/components/custom/AboutMe';
 import TasbihList from './src/components/custom/TasbihList';
-// import TeacherMessage from './src/components/custom/TeacherMessage'; // Students create this
+import TeacherMessage from './src/components/custom/TeacherMessage';
 import SearchAndAdd from './src/components/custom/SearchAndAdd';
 import { STUDENT_NAME, REG_NO } from './src/config/student';
+import { initialAzkaar } from './src/data/azkaar';
 
 export default function App() {
+  const [azkaar, setAzkaar] = useState(initialAzkaar);
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <AboutMe name={STUDENT_NAME} regNo={REG_NO} />
-      {/* TODO: after creating the custom/TeacherMessage component, render it here */}
-      <TasbihList />
-      <SearchAndAdd />
+      <TeacherMessage />
+      <TasbihList azkaar={azkaar} setAzkaar={setAzkaar} />
+      <SearchAndAdd azkaar={azkaar} setAzkaar={setAzkaar} />
     </ScrollView>
   );
 }
